15fd2e73ce60bffc5002a97db375750797f86167
Vishal <vishal.koosuri1@marist.edu>
2017-12-23 07:10:44 +0000

    Commented out some code

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')),
-    url(r'^https://intense-plains-76355.herokuapp.com', include('blog.urls', namespace='intense-plains-76355', app_name='intense-plains-76355')),
-    
 ]
